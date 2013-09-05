17eff262fa030b75cf4331517bb459919af9562f
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2013-09-05 10:17:19 +0400

    whitelist support added to iptables scripts

--- iptables-block-gosnet.sh
+++ iptables-block-gosnet.sh
@@ -4,18 +4,33 @@
 
 case "$1" in
 	start)
+		# Creating a chain
 		iptables -t filter -N BLOCKGOSNET
 		iptables -t filter -A BLOCKGOSNET -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+		# Whitelist
+		getwhitelist | while read net; do
+			iptables -t filter -A BLOCKGOSNET -s "$net" -m comment --comment "Блокировка госорганов" -j RETURN
+		done
+
+		# Blacklist
 		getblacklist | while read net; do
 			iptables -t filter -A BLOCKGOSNET -s "$net" -m comment --comment "Блокировка госорганов" -j DROP
 		done
+
+		# Enabling
 		iptables -t filter -I FORWARD -j BLOCKGOSNET
 		iptables -t filter -I INPUT   -j BLOCKGOSNET
 		;;
 	stop)
+		# Disabling
 		iptables -t filter -D FORWARD -j BLOCKGOSNET
 		iptables -t filter -D INPUT   -j BLOCKGOSNET
+
+		# Cleaning the chain
 		iptables -t filter -F BLOCKGOSNET
+
+		# Removing the chain
 		iptables -t filter -X BLOCKGOSNET
 		;;
 	restart)
